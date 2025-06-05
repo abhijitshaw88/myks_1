ef3f4fc2e2a6a43ab3740923f14597710827f0e1
abhijit shaw <abhijitshaw88@gmail.com>
2025-06-06 00:52:29 +0530

    Build issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Header from './components/layout/Header';
 import Hero from './components/home/Hero';
 import Services from './components/services/Services';
@@ -9,12 +8,7 @@ import FAQ from './components/faq/FAQ';
 import Contact from './components/contact/Contact';
 
 const App = () => {
-  const [activeFaq, setActiveFaq] = useState(null);
-
-  const toggleFaq = (index) => {
-    setActiveFaq(activeFaq === index ? null : index);
-  };
-
+  
   return (
     <div className="font-sans bg-gray-50">
       <Header />
